c519c6c2dd2ebf5a58a1c9f0c3cf3f2fff355e8b
ajuvercr <arthur.vercruysse@outlook.com>
2019-05-27 16:08:56 +0200

    ws

--- src/main.rs
+++ src/main.rs
@@ -1,54 +1,16 @@
-extern crate tokio;
+extern crate ws;
 
-use tokio::io;
-use tokio::net::TcpListener;
-use tokio::prelude::*;
+use ws::listen;
 
 use std::env;
 
 fn main() {
     let port = env::var("PORT").unwrap_or(String::from("80"));
-    let addr = format!("0.0.0.0:{}", port).parse().unwrap();
-    let listener = TcpListener::bind(&addr).unwrap();
+    let addr = format!("0.0.0.0:{}", port);
 
-    // Here we convert the `TcpListener` to a stream of incoming connections
-    // with the `incoming` method. We then define how to process each element in
-    // the stream with the `for_each` combinator method
-    let server = listener.incoming().for_each(|socket| {
-    // split the socket stream into readable and writable parts
-        let (reader, writer) = socket.split();
-        // copy bytes from the reader into the writer
-        let amount = io::copy(reader, writer);
-
-        let msg = amount.then(|result| {
-            match result {
-                Ok((amount, _, _)) => println!("wrote {} bytes", amount),
-                Err(e)             => println!("error: {}", e),
-            }
-
-            Ok(())
-        });
-
-        // spawn the task that handles the client connection socket on to the
-        // tokio runtime. This means each client connection will be handled
-        // concurrently
-        tokio::spawn(msg);
-        Ok(())
-    })
-    .map_err(|err| {
-        // Handle error by printing to STDOUT.
-        println!("accept error = {:?}", err);
-    });
-
-    println!("server running on 0.0.0.0:{}", port);
-
-    // Start the server
-    //
-    // This does a few things:
-    //
-    // * Start the Tokio runtime
-    // * Spawns the `server` task onto the runtime.
-    // * Blocks the current thread until the runtime becomes idle, i.e. all
-    //   spawned tasks have completed.
-    tokio::run(server);
+    listen(&addr, |out| {
+        move |msg| {
+        out.send(msg)
+    }
+    }).unwrap()
 }

5c8aacb40d11f31d6146bea580980e80cd2cc416
ajuvercr <arthur.vercruysse@ugent.be>
2019-05-30 21:17:23 +0200

    hm, workinggit add src/ now the other server

--- src/bin/client/main.rs
+++ src/bin/client/main.rs
@@ -1,27 +1,26 @@
 
-#[macro_use]
 extern crate futures;
 extern crate tokio_core;
 extern crate tokio_io;
 extern crate tokio;
 extern crate bert;
+extern crate websocket;
 
-use ws::{connect, Message, Sender, Handler};
+use websocket::client::ClientBuilder;
+use websocket::receiver::{Reader};
+use websocket::sender::Writer;
 
 use futures::Future;
 use futures::stream::Stream;
 use futures::prelude::*;
-use futures::future::lazy;
 
 use tokio_io::{AsyncRead, AsyncWrite};
-use tokio_io::io::{ReadHalf, WriteHalf};
 use tokio_core::net::{TcpListener};
 use tokio_core::reactor::Core;
 
-use std::io::{self, Write};
+use std::io::{self};
 use std::env;
-use std::sync::{Arc, Mutex};
-use std::mem;
+use std::net::TcpStream;
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -39,99 +38,92 @@ fn main() -> io::Result<()> {
     let socket = TcpListener::bind(&inner_address, &handle).unwrap();
     println!("Listening on: {:?}", inner_address);
 
-    let done = socket.incoming().for_each(move |(socket, addr)| {
+    let done = socket.incoming()
+        .map(move |(socket, addr)| {
+            println!("Got connection from {:?}", addr);
+            let client = ClientBuilder::new(&outer_address.clone())
+                .unwrap()
+                .add_protocol("rust-websocket")
+                .connect_insecure()
+                .unwrap();
+            (socket, client)
+        })
+        .for_each(move |(tcp_socket, ws_socket)| {
+            println!("Extra connection made");
 
-        println!("got conntection from {}", addr);
-        let socket = Arc::new(Mutex::new(Some(socket)));
-        let outer_address = outer_address.clone();
+            let (tcp_tx, tcp_rx) = tcp_socket.split();
+            let (ws_tx, ws_rx) = ws_socket.split().unwrap();
 
-        tokio::spawn(lazy(
-            move || {
+            tokio::spawn(tokio::io::copy(tcp_tx, WSWriter(ws_rx)).map(|(size, _, _)| println!("copied {} bytes", size)).map_err(|e| eprintln!("oops {:?}", e)));
+            tokio::spawn(tokio::io::copy(WSReader(ws_tx), tcp_rx).map(|(size, _, _)| println!("copied {} bytes", size)).map_err(|e| eprintln!("oops {:?}", e)));
 
-                connect(outer_address.clone(), move |out| {
-                    println!("connecting to {}", outer_address.clone());
-
-                    let socket = mem::replace(
-                        &mut *socket.clone().lock().map_err(|_| ()).unwrap(), 
-                        None
-                    ).unwrap();
-
-                    let (reader, writer) = socket.split();
-
-                    tokio::spawn(
-                        WriteFuture::new(reader, out.clone())
-                    );
-
-                    Writer { writer }
-                }).map_err(|e| eprintln!("failed {:?}",e )).unwrap();
-                Ok(())
-            }
-        ));
-
-        Ok(())
-    });
-
+            Ok(())
+        });
 
     core.run(done).unwrap();
 
     Ok(())
 }
 
-struct Writer<S> {
-    writer: WriteHalf<S>
-}
+struct WSReader(Reader<TcpStream>);
 
-impl<S> Handler for Writer<S> 
-    where S: AsyncWrite {
+impl AsyncRead for WSReader {
 
-    fn on_message(&mut self, msg: Message) -> ws::Result<()> {
-        println!("got message {:?}", msg);
-
-        match msg {
-            Message::Binary(bin) => self.writer.write_all(&bin),
-            Message::Text(msg) => self.writer.write_all(msg.as_bytes()),
-        }.expect("Couldn't write to tcp stream");
-
-        Ok(())
+    unsafe fn prepare_uninitialized_buffer(&self, _: &mut [u8]) -> bool {
+        false
     }
 }
 
-struct WriteFuture<S> {
-    reader: ReadHalf<S>,
-    sender: Sender,
-}
+impl io::Read for WSReader {
 
-impl<S> WriteFuture<S> {
-    fn new(reader: ReadHalf<S>, sender: Sender) -> Self 
-        where S: std::fmt::Debug {
-        WriteFuture {
-            reader,
-            sender,
-        }
-    }
-}
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
 
-impl<S> Future for WriteFuture<S>
-    where S: AsyncRead {
-        type Item = ();
-        type Error = ();
-
-    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
-
-        loop {
-            let mut buffer = Vec::new();
-
-            let size = try_ready!(self.reader.read_buf(&mut buffer).map_err(|e| eprintln!("{:?}", e)));
-
+        if let Ok(size) = self.0.stream.get_ref().peek(buf) {
             if size == 0 {
-                return Ok(Async::NotReady);
+                return Ok(0)
             }
-
-            println!("Got buffer {:?}", buffer);
-
-            self.sender.send(Message::Binary(buffer))
-                .map_err(|e| eprintln!("{:?}", e)).unwrap();
         }
+
+        self.0.recv_message().map(|msg| {
+            match msg {
+                message::OwnedMessage::Binary(bin) => {
+                    buf[..bin.len()].copy_from_slice(&bin);
+                    bin.len()
+                },
+                _ => {
+                    eprintln!("euhm erroring? Got message {:?}", msg);
+                    0
+                }
+            }
+        })
+        .map_err(|e| {eprintln!("error {:?}", e); io::Error::new(io::ErrorKind::BrokenPipe, "woopsy")})
     }
 }
 
+struct WSWriter<R: io::Write>(Writer<R>);
+
+impl<R> AsyncWrite for WSWriter<R>
+    where R: io::Write {
+
+    fn shutdown(&mut self) -> Poll<(), io::Error> {
+        Ok(Async::Ready(()))
+    }
+}
+
+use websocket::message;
+impl<R> io::Write for WSWriter<R>
+    where R: io::Write  {
+
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        let buf = buf.to_vec();
+        println!("Writing {:?}", buf);
+        let length = buf.len();
+        self.0.send_message(&message::OwnedMessage::Binary(buf))
+            .map(|_| length)
+            .map_err(|e| {eprintln!("error {:?}", e); io::Error::new(io::ErrorKind::BrokenPipe, "woopsy")})
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.0.stream.flush()
+    }
+}
